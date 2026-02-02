b29d43685c8c598e8811dd10211ce2057924db7a
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2026-02-01 21:20:57 -0600

    Agregando los test de los casos faltantes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,99 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
+    private IPostfixCalculator calc;
+
+    @BeforeEach
+    public void setUp() {
+        IStack<Integer> stack = new VectorStack<>();
+        calc = new PostfixCalculator(stack);
+    }
+
     @Test
     public void testExpression1() {
-        IStack<Integer> stack = new VectorStack<>();
-        IPostfixCalculator calc = new PostfixCalculator(stack);
-
         assertEquals(15, calc.evaluate("1 2 + 4 * 3 +"));
     }
 
     @Test
     public void testExpression2() {
-        IStack<Integer> stack = new VectorStack<>();
-        IPostfixCalculator calc = new PostfixCalculator(stack);
-
         assertEquals(30, calc.evaluate("6 2 3 + *"));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(3, calc.evaluate("10 7 -"));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(5, calc.evaluate("20 4 /"));
+    }
+
+    @Test
+    public void testModulo() {
+        assertEquals(1, calc.evaluate("10 3 %"));
+    }
+
+    @Test
+    public void testMultipleOperations() {
+        assertEquals(14, calc.evaluate("5 1 2 + 4 * + 3 -"));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        assertEquals(42, calc.evaluate("42"));
+    }
+
+    @Test
+    public void testMultiDigitNumbers() {
+        assertEquals(111, calc.evaluate("100 11 +"));
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.evaluate("10 0 /"));
+    }
+
+    @Test
+    public void testModuloByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.evaluate("10 0 %"));
+    }
+
+    @Test
+    public void testInvalidSymbol() {
+        assertThrows(IllegalArgumentException.class, () -> calc.evaluate("1 2 @"));
+    }
+
+    @Test
+    public void testInvalidCharacter() {
+        assertThrows(IllegalArgumentException.class, () -> calc.evaluate("1 a +"));
+    }
+
+    @Test
+    public void testInsufficientOperands() {
+        assertThrows(IllegalArgumentException.class, () -> calc.evaluate("1 +"));
+    }
+
+    @Test
+    public void testInsufficientOperandsSingleOperator() {
+        assertThrows(IllegalArgumentException.class, () -> calc.evaluate("+"));
+    }
+
+    @Test
+    public void testInvalidExpressionTooManyOperands() {
+        assertThrows(IllegalArgumentException.class, () -> calc.evaluate("1 2 3 +"));
+    }
+
+    @Test
+    public void testNonCommutativeSubtraction() {
+        assertEquals(2, calc.evaluate("5 3 -"));
+    }
+
+    @Test
+    public void testNonCommutativeDivision() {
+        assertEquals(5, calc.evaluate("10 2 /"));
     }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.EmptyStackException;
 
 public class StackTest {
 
@@ -18,5 +19,74 @@ public class StackTest {
     public void testIsEmpty() {
         IStack<Integer> stack = new VectorStack<>();
         assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyAfterPush() {
+        IStack<Integer> stack = new VectorStack<>();
+        stack.push(5);
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    public void testPeek() {
+        IStack<Integer> stack = new VectorStack<>();
+        stack.push(10);
+        stack.push(20);
+
+        assertEquals(20, stack.peek());
+        assertEquals(20, stack.peek());
+        assertEquals(2, stack.size());
+    }
+
+    @Test
+    public void testSize() {
+        IStack<Integer> stack = new VectorStack<>();
+        assertEquals(0, stack.size());
+
+        stack.push(1);
+        assertEquals(1, stack.size());
+
+        stack.push(2);
+        stack.push(3);
+        assertEquals(3, stack.size());
+
+        stack.pop();
+        assertEquals(2, stack.size());
+    }
+
+    @Test
+    public void testClear() {
+        IStack<Integer> stack = new VectorStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        stack.clear();
+
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+    }
+
+    @Test
+    public void testPopEmptyStack() {
+        IStack<Integer> stack = new VectorStack<>();
+        assertThrows(EmptyStackException.class, () -> stack.pop());
+    }
+
+    @Test
+    public void testPeekEmptyStack() {
+        IStack<Integer> stack = new VectorStack<>();
+        assertThrows(EmptyStackException.class, () -> stack.peek());
+    }
+
+    @Test
+    public void testDynamicResize() {
+        IStack<Integer> stack = new VectorStack<>();
+        for (int i = 0; i < 15; i++) {
+            stack.push(i);
+        }
+        assertEquals(15, stack.size());
+        assertEquals(14, stack.pop());
     }
 }
